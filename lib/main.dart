a6a620fdc62ed9252daa8edd3b01e272fb13e483
Elí Tp <eli.tp.system@example.com>
2023-02-22 18:26:12 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:codigo6_shared/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  save() async {
+    SharedPreferences mandarina = await SharedPreferences.getInstance();
+  }
+
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: HomePage(),
+    );
+  }
+}
